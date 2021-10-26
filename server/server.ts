748e6301afbfb53afd0f88f39d609c36c33368d3
Lomacker <AndreyCyn@gmail.com>
2021-10-26 15:45:33 +0300

    sequelize connect server

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,24 @@
-const express = require('express')
-const next = require('next')
+const express = require('express');
+const next = require('next');
+const { Sequelize } = require('sequelize');
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const sequelize = new Sequelize('librarydb', 'root', 'qwerty',
+  {
+    host: 'localhost',
+    dialect: 'mysql'
+  });
+
+try {
+  sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+};
 
 app.prepare().then(() => {
   const server = express()
@@ -17,6 +31,10 @@ app.prepare().then(() => {
     if (err) throw err
     console.log(`> Ready on http://localhost:${port}`)
   })
-})
+});
+
+
+
+
 
 export default {};
